c95bb799944fb9aa3cc832d81be0e4aff9f3155d
janeyaLouvelle-turns7 <jowett.cenena@gmail.com>
2024-12-02 21:47:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const countdownDate = new Date("Dec 15, 2024 00:00:00").getTime();
+// Set the date of the birthday party
+const countdownDate = new Date("Dec 15, 2024 15:00:00").getTime();
 
-const timer = setInterval(function() {
+const timer = setInterval(() => {
     const now = new Date().getTime();
     const distance = countdownDate - now;
 
@@ -9,7 +10,7 @@ const timer = setInterval(function() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    document.getElementById("timer").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
     if (distance < 0) {
         clearInterval(timer);
